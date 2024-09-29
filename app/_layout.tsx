bb3754f93e80fbe886f447d02ead3613b1a59b51
ttchien <chientt.00.student@fit.tdc.edu.vn>
2024-09-29 15:09:40 +0700

    doing single quiz

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -50,6 +50,12 @@ export default function RootLayout() {
 							options={{ headerShown: false }}
 						/>
 						<Stack.Screen name="+not-found" />
+						<Stack.Screen
+							name="SinglePlay"
+							options={{ headerShown: false }}
+							
+						/>
+
 					</Stack>
 				</UserProvider>
 			</AppProvider>
