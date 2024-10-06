403c2d7025ef28d945811a6456af89c2242f57c0
Datto098 <dat61222@gmail.com>
2024-10-06 22:44:52 +0700

    update quiz

--- utils/index.jsx
+++ utils/index.jsx
@@ -1,18 +1,30 @@
 'use strict';
 
 const validateEmail = (email) => {
-    const re = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
-    return re.test(email);
+	const re = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+	return re.test(email);
 };
 
 const validatePassword = (password) => {
-    // password must be at least 8 characters long and contain at least one number
-    const re = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
-    return re.test(password);
-}
-
-const validateFullname = (fullname) => {
-    return 1;
+	// password must be at least 8 characters long and contain at least one number
+	const re = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+	return re.test(password);
 };
 
-export { validateEmail, validatePassword,validateFullname };
+const validateFullname = (fullname) => {
+	return 1;
+};
+
+const convertSubjectData = (data) => {
+	return data.map((item, index) => ({
+		key: item._id.toString(),
+		value: item.name,
+	}));
+};
+
+export {
+	validateEmail,
+	validatePassword,
+	validateFullname,
+	convertSubjectData,
+};
