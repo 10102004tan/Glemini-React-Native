5b4d09c8fbd8c92bed120ebf7e9fe62f9e320cb2
ttchien <chientt.00.student@fit.tdc.edu.vn>
2024-10-06 14:16:51 +0700

    doing API review result

---
+++ app/(app)/(result)/review.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text, Image } from 'react-native';
+
+const ResultReview = () => {
+
+	return (
+		<View className="flex-1 bg-[#1C2833] px-5 pb-4 pt-10">
+			
+		</View>
+	);
+};
+
+export default ResultReview;
