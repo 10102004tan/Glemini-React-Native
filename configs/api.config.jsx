c903560ade28ce57cc280641f64b174b735dcd9b
Chau23445 <huynhchau.04014@gmail.com>
2024-10-07 12:37:48 +0700

    upload

--- configs/api.config.jsx
+++ configs/api.config.jsx
@@ -1,44 +1,44 @@
-'use strict';
+"use strict";
 
 const dev = {
-	API_URL: 'http://192.168.1.8:8000/api',
-	API_VERSION: {
-		V1: '/v1',
-	},
-	END_POINTS: {
-		LOGIN: '/auth/login',
-		SIGN_UP: '/auth/signup',
-		LOGOUT: '/auth/logout',
-		REFRESH_TOKEN: '/refresh-token',
-		PROFILE: '/auth/profile',
-		CHANGE_PASSWORD: '/auth/change-password',
-		USER_STATUS: '/auth/status',
-		GET_QUIZ_BY_USER: '/quizzes/get-by-user',
-		GET_QUIZ_QUESTIONS: '/quizzes/get-questions',
-		GET_QUESTION_DETAIL: '/questions/get-details',
-		QUESTION_UPDATE: '/questions/update',
-		QUESTION_CREATE: '/questions/create',
-		QUIZ_DETAIL: '/quizzes/get-details',
-		QUIZ_CREATE: '/quizzes/create',
-		QUIZ_DELETE: '/quizzes/delete',
-		QUIZ_UPDATE: '/quizzes/update',
-		RESULT_SAVE_QUESTION: '/result/save-question',
-		RESULT_COMPLETED: '/result/complete-quiz',
-		SUBJECTS: '/subjects',
-	},
+  API_URL: "http://192.168.1.9:8000/api",
+  API_VERSION: {
+    V1: "/v1",
+  },
+  END_POINTS: {
+    LOGIN: "/auth/login",
+    SIGN_UP: "/auth/signup",
+    LOGOUT: "/auth/logout",
+    REFRESH_TOKEN: "/refresh-token",
+    PROFILE: "/auth/profile",
+    CHANGE_PASSWORD: "/auth/change-password",
+    USER_STATUS: "/auth/status",
+    GET_QUIZ_BY_USER: "/quizzes/get-by-user",
+    GET_QUIZ_QUESTIONS: "/quizzes/get-questions",
+    GET_QUESTION_DETAIL: "/questions/get-details",
+    QUESTION_UPDATE: "/questions/update",
+    QUESTION_CREATE: "/questions/create",
+    QUIZ_DETAIL: "/quizzes/get-details",
+    QUIZ_CREATE: "/quizzes/create",
+    QUIZ_DELETE: "/quizzes/delete",
+    QUIZ_UPDATE: "/quizzes/update",
+    RESULT_SAVE_QUESTION: "/result/save-question",
+    RESULT_COMPLETED: "/result/complete-quiz",
+    SUBJECTS: "/subjects",
+  },
 };
 
 const prod = {
-	API_VERSION: {
-		V1: '/v1',
-	},
-	API_URL: 'http://localhost:8000/api',
-	END_POINTS: {
-		LOGIN: '/login',
-		REGISTER: '/register',
-		LOGOUT: '/logout',
-		PROFILE: '/profile',
-	},
+  API_VERSION: {
+    V1: "/v1",
+  },
+  API_URL: "http://localhost:8000/api",
+  END_POINTS: {
+    LOGIN: "/login",
+    REGISTER: "/register",
+    LOGOUT: "/logout",
+    PROFILE: "/profile",
+  },
 };
 
 module.exports = dev;
