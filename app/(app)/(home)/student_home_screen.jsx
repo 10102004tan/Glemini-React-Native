393072415379b730465208073831c83cb43dabe9
ttchien <chientt.00.student@fit.tdc.edu.vn>
2024-10-10 09:53:42 +0700

    doing student home UI

--- app/(app)/(home)/student_home_screen.jsx
+++ app/(app)/(home)/student_home_screen.jsx
@@ -1,11 +1,46 @@
-import { View, Text } from 'react-native';
+import { View, Text, Dimensions, Image } from 'react-native';
 import React from 'react';
+import Wrapper from '@/components/customs/Wrapper';
+import Carousel from 'react-native-reanimated-carousel';
+import { Images } from '@/constants';
+import { useAppProvider } from '@/contexts/AppProvider';
 
 const StudentHomeScreen = () => {
+	const {i18n} = useAppProvider()
+	const dataSet = [
+		Images.banner1,
+		Images.banner2,
+		Images.banner3
+	]
+	const width = Dimensions.get('window').width
+
 	return (
-		<View>
-			<Text>StudentHomeScreen</Text>
-		</View>
+		<Wrapper>
+			<View className='mt-6'>
+				<Carousel
+					loop
+					width={width}
+					height={width * 2 / 3}
+					autoPlay={true}
+					data={dataSet}
+					mode='parallax'
+					scrollAnimationDuration={2500}
+					renderItem={({ item }) => (
+						<Image
+							source={item}
+							className="w-full h-full rounded-2xl"
+							style={{ resizeMode: 'cover' }}
+						/>
+					)}
+				/>
+			</View>
+
+			<View className='px-4'>
+				<Text className='text-2xl font-bold'>{i18n.t('student_homepage.exprole')}</Text>
+				
+			</View>
+		</Wrapper>
+
 	);
 };
 
--- constants/images.js
+++ constants/images.js
@@ -1,5 +1,8 @@
 import woman from '../assets/images/woman 1.png';
+import banner1 from '../assets/images/banner-1.png'
+import banner2 from '../assets/images/banner-2.png'
+import banner3 from '../assets/images/banner-3.png'
 
 export default {
-	woman,
+	woman, banner1, banner2, banner3
 };
