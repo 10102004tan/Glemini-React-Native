34959291768ae74946d3680a9f2c334fb0127105
Nguyen Phuong Tan <tannguyen.10102004@gmail.com>
2024-10-25 12:53:02 +0700

    PUSH:: feature notification

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,17 +1,22 @@
 import { useFonts } from 'expo-font';
 import { Slot } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
+import {useEffect, useRef, useState} from 'react';
 import 'react-native-reanimated';
 import Toast from 'react-native-toast-message';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { LogBox } from 'react-native';
 import Providers from '@/contexts/Providers';
+import { Text, View, Button, Platform } from 'react-native';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 // Bỏ qua cảnh báo chứa chuỗi "defaultProps"
 LogBox.ignoreLogs(['defaultProps']);
+import Constants from 'expo-constants';
+import * as Notifications from 'expo-notifications';
+
+const token = (await Notifications.getDevicePushTokenAsync()).data;
 
 export default function RootLayout() {
 	const [loaded] = useFonts({
@@ -26,7 +31,11 @@ export default function RootLayout() {
 		'Poppins-Thin': require('../assets/fonts/Poppins-Thin.ttf'),
 	});
 
+
+
+
 	useEffect(() => {
+		console.log('token', token);
 		if (loaded) {
 			SplashScreen.hideAsync();
 		}
@@ -44,4 +53,63 @@ export default function RootLayout() {
 			</Providers>
 		</GestureHandlerRootView>
 	);
+}
+
+async function schedulePushNotification() {
+	await Notifications.scheduleNotificationAsync({
+		content: {
+			title: "You've got mail! 📬",
+			body: 'Here is the notification body',
+			data: { data: 'goes here', test: { test1: 'more data' } },
+		},
+		trigger: { seconds: 2 },
+	});
+}
+
+async function registerForPushNotificationsAsync() {
+	let token;
+
+	if (Platform.OS === 'android') {
+		await Notifications.setNotificationChannelAsync('default', {
+			name: 'default',
+			importance: Notifications.AndroidImportance.MAX,
+			vibrationPattern: [0, 250, 250, 250],
+			lightColor: '#FF231F7C',
+		});
+	}
+
+	if (Device.isDevice) {
+		const { status: existingStatus } = await Notifications.getPermissionsAsync();
+		let finalStatus = existingStatus;
+		if (existingStatus !== 'granted') {
+			const { status } = await Notifications.requestPermissionsAsync();
+			finalStatus = status;
+		}
+		if (finalStatus !== 'granted') {
+			alert('Failed to get push token for push notification!');
+			return;
+		}
+		// Learn more about projectId:
+		// https://docs.expo.dev/push-notifications/push-notifications-setup/#configure-projectid
+		// EAS projectId is used here.
+		try {
+			const projectId =
+				Constants?.expoConfig?.extra?.eas?.projectId ?? Constants?.easConfig?.projectId;
+			if (!projectId) {
+				throw new Error('Project ID not found');
+			}
+			token = (
+				await Notifications.getExpoPushTokenAsync({
+					projectId,
+				})
+			).data;
+			console.log(token);
+		} catch (e) {
+			token = `${e}`;
+		}
+	} else {
+		alert('Must use physical device for Push Notifications');
+	}
+
+	return token;
 }
--- utils/socket.js
+++ utils/socket.js
@@ -1,4 +1,4 @@
 'use strict'
 import { io } from "socket.io-client";
-const socket = io.connect("http://10.0.249.255:3000");
+const socket = io.connect("http://10.0.226.182:8000");
 export default socket;
