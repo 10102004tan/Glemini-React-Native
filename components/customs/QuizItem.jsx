cc8b3bb53cffbe895dc5209beb0e01b9c23cc10e
ttchien <chientt.00.student@fit.tdc.edu.vn>
2024-11-14 12:51:49 +0700

    UPDATE:: update code branch

--- components/customs/QuizItem.jsx
+++ components/customs/QuizItem.jsx
@@ -8,15 +8,19 @@ import moment from 'moment'
 function QuizItem({ quiz }) {
     const { i18n } = useAppProvider()
     return (
-        <View className='bg-slate-100 rounded-lg'>
+        <View className='bg-slate-50 rounded-lg'>
             <View>
                 <Image src={(quiz.quiz_thumb ? quiz.quiz_thumb : "https://elearningindustry.com/wp-content/uploads/2021/10/Shareable-Quizzes-In-Online-Training-7-Reasons.jpg")} className={"w-full h-[100px] rounded-b-[10px]"} alt={quiz.quiz_name} />
                 <Text className={"absolute p-2 rounded bg-white opacity-80 text-[10px] bottom-[5px] right-[5px]"}>{moment(quiz.createdAt).fromNow()}</Text>
             </View>
-            <View className="p-2">
-                <Text>{quiz.quiz_name}</Text>
-                <Text>Lượt chơi : {quiz.quiz_turn}</Text>
+            <View className={"p-2"}>
+            <Text>{(quiz.quiz_name.length > 25 ? quiz.quiz_name.substring(0, 25) + "..." : quiz.quiz_name)}</Text>
+            <View className={"flex-row gap-2 items-center mt-3"}>
+                <Image className={"w-[20px] h-[20px] rounded-full object-cover"}
+                       src={quiz.user_id.user_avatar.replace("h_100", "h_30").replace("w_100", "w_30")}/>
+                <Text className={"text-[10px]"}>{quiz.user_id.user_fullname}</Text>
             </View>
+        </View>
         </View>
     )
 }
