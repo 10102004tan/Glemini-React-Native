1189cab71a23885881c100a9933e4b98d7b19a12
ttchien <chientt.00.student@fit.tdc.edu.vn>
2024-11-17 20:10:21 +0700

    UPDATE:: update code UI review result

--- app/(app)/(home)/activity.jsx
+++ app/(app)/(home)/activity.jsx
@@ -32,7 +32,6 @@ export default function ActivityScreen() {
    useFocusEffect(
       useCallback(() => {
          fetchResultsForStudent();
-         setIndex(0)
       }, [])
    );
 
@@ -257,7 +256,7 @@ const ResultDoingItem = ({ result }) => {
             {(result.quiz_id?.quiz_name.length > 20 ? result.quiz_id?.quiz_name.substring(0, 20) + "..." : result.quiz_id?.quiz_name)}
          </Text>
          <Text className="text-xs font-light">
-            hạn: {moment(result.exercise_id.date_end).format('DD/MM/YYYY')}
+            hạn: {moment(result.exercise_id?.date_end).format('DD/MM/YYYY')}
          </Text>
 
          <Text className="text-sm mt-4 font-light text-center text-slate-50 bg-violet-300 rounded-full px-2">
