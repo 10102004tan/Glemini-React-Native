a0f280f368c1144bb6cdc7e0e151451fe83abe77
Datto098 <dat61222@gmail.com>
2024-11-18 12:57:37 +0700

    UPDATE: update

--- configs/api.config.js
+++ configs/api.config.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const dev = {
-	API_URL: 'http://192.168.1.16:3000/api',
+	API_URL: 'http://98.83.149.250:8000/api',
 	API_VERSION: {
 		V1: '/v1',
 	},
--- utils/socket.js
+++ utils/socket.js
@@ -1,6 +1,6 @@
 'use strict';
 import { io } from 'socket.io-client';
-const socket = io.connect('http://192.168.1.16:3000', {
+const socket = io.connect('http://98.83.149.250:8000', {
 	transports: ['websocket'],
 	reconnection: true,
 });
