d3e1c68e808368378d5df206e88150a527e97db2
Chau23445 <huynhchau.04014@gmail.com>
2024-12-09 14:13:56 +0700

    update quizShared

--- utils/socket.js
+++ utils/socket.js
@@ -1,6 +1,6 @@
 'use strict';
 import { io } from 'socket.io-client';
-const socket = io.connect("http://172.20.10.9:3000", {
+const socket = io.connect("http://18.139.228.246:3000", {
   transports: ["websocket"],
   reconnection: true,
 });
