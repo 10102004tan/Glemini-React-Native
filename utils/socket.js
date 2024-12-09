58cf41850cabc3e78b89bb6dabddce84fbd6f359
Chau23445 <huynhchau.04014@gmail.com>
2024-12-09 12:52:29 +0700

    update

--- utils/socket.js
+++ utils/socket.js
@@ -1,6 +1,6 @@
 'use strict';
 import { io } from 'socket.io-client';
-const socket = io.connect("http://192.168.1.19:3000", {
+const socket = io.connect("http://192.168.2.65:3000", {
   transports: ["websocket"],
   reconnection: true,
 });
