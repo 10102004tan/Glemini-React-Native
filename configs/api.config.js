dd43c9a0944d8ba684ab0bc00972ff6c741730be
Nguyen Tien Dat <datto2442004@gmail.com>
2025-06-03 13:53:10 +0700

    INIT::

--- configs/api.config.js
+++ configs/api.config.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const dev = {
-	API_URL: 'http://192.168.178.179:3000/api',
+	API_URL: 'http://192.168.2.4:8000/api',
 	API_VERSION: {
 		V1: '/v1',
 	},
--- utils/socket.js
+++ utils/socket.js
@@ -1,6 +1,6 @@
 'use strict';
 import { io } from 'socket.io-client';
-const socket = io.connect('http://192.168.178.179:3000', {
+const socket = io.connect('http://192.168.2.4:8000', {
 	transports: ['websocket'],
 	reconnection: true,
 });
